040642592fe5b6c23d415f44bd0ac1e340179a80
pritisekhsaria <56923247+pritisekhsaria@users.noreply.github.com>
2021-06-07 22:02:00 +0530

    Add files via upload

---
+++ formValidate1.js
@@ -0,0 +1,55 @@
+function validate()
+{
+	var flag=true;
+	var str="";
+	//checking for name
+	if(document.forms[0].elements[1].value.length < 1)
+	{
+		flag=false;
+		str=str+"\n Name cannot be blank";
+	}
+	
+	//checking for password
+	if(document.getElementById("pwd").value.length < 1)
+	{
+		flag=false;
+		str=str+"\n Password cannot be blank";
+	}
+	
+	//checking for gender
+	var x=document.forms[0].gender[0].checked; // will save true if checked
+	var y=document.forms[0].gender[1].checked;
+	if(x==false && y==false)
+	{	
+		flag = false;
+		str = str + "\n Please enter a gender";
+	}
+	
+	//checking for hobbies
+	var a=document.forms[0].hobbies[0].checked;
+	var b=document.forms[0].hobbies[1].checked;
+	var c=document.forms[0].hobbies[2].checked;
+	if(a==false && b==false && c==false)
+	{
+		flag = false;
+		str=str + "\n Please enter a hobby";
+	}
+
+	//checking for city
+	var city=document.f1.c1.value;
+	if( city=="")	//"" is the value of "select city" option.
+	{
+		flag= false;
+		str=str + "\n Please enter a city";
+	} 
+
+	
+	if(flag==false)
+	{
+		alert(str);
+	}
+	else
+	{
+		document.forms[0].submit();
+	}
+}
